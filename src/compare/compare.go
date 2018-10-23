5e9ad34c84239fc59786189084db3b5fd62c731a
Lorna Song <lorna.song@gmail.com>
2018-10-22 20:45:06 -0400

    initial pass at matching lines across two pieces of text

---
+++ src/compare/compare.go
@@ -0,0 +1,85 @@
+package compare
+
+import (
+	"strings"
+)
+
+// LineMatch contains text from string a that has been matched to string
+// b up until newline. No match is represented by an empty string
+type LineMatch struct {
+	a string
+	b string
+}
+
+// NewLineMatch returns a new LineMatch
+func NewLineMatch(a, b string) *LineMatch {
+	return &LineMatch{
+		a: a,
+		b: b,
+	}
+}
+
+// A returns the value of string a
+func (p *LineMatch) A() string {
+	return p.a
+}
+
+// B returns the value of string b
+func (p *LineMatch) B() string {
+	return p.b
+}
+
+// Match returns a list of LineMatchs that represent the matches between string
+// a and string b if any.
+func Match(a, b string) []*LineMatch {
+
+	aPieces := strings.Split(a, "\n")
+	bPieces := strings.Split(b, "\n")
+
+	matches := make([]*LineMatch, 0)
+	unmatchedB := make([]*LineMatch, 0)
+	matched := false
+
+	// TODO: refactor. think through any optimizations
+	for _, a := range aPieces {
+		matched = false
+		unmatchedB = nil
+
+		// look for matches to b
+		for ixb, b := range bPieces {
+
+			if !sameLine(a, b) {
+				unmatchedB = append(unmatchedB, NewLineMatch("", b))
+				continue
+			}
+
+			if len(unmatchedB) > 0 {
+				matches = append(matches, unmatchedB...)
+				unmatchedB = nil
+			}
+
+			matched = true
+			matches = append(matches, NewLineMatch(a, b))
+
+			// trim off added stuff
+			bPieces = bPieces[ixb+1:]
+			break
+		}
+		// there was no match to b
+		if !matched {
+			matches = append(matches, NewLineMatch(a, ""))
+		}
+	}
+
+	// incomplete b
+	for _, b := range bPieces {
+		matches = append(matches, NewLineMatch("", b))
+	}
+
+	return matches
+}
+
+func sameLine(a, b string) bool {
+	// grow in complexity to be "similarLine"
+	return a == b
+}
---
+++ src/compare/compare_test.go
@@ -0,0 +1,163 @@
+package compare_test
+
+import (
+	"fmt"
+	"testing"
+
+	comp "github.com/lornasong/diff-checker/src/compare"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMatch_Same(t *testing.T) {
+	cases := []struct {
+		scenario string
+		str      string
+		expected []*comp.LineMatch
+	}{
+		{
+			scenario: "Same, No Newline",
+			str:      "abcd",
+			expected: []*comp.LineMatch{
+				comp.NewLineMatch("abcd", "abcd"),
+			},
+		},
+		{
+			scenario: "Same, With Single Newline, Basic",
+			str:      "abcd\n1234",
+			expected: []*comp.LineMatch{
+				comp.NewLineMatch("abcd", "abcd"),
+				comp.NewLineMatch("1234", "1234"),
+			},
+		},
+		{
+			scenario: "Same, With Multi Newline",
+			str:      "abcd\n1234\nqwerty",
+			expected: []*comp.LineMatch{
+				comp.NewLineMatch("abcd", "abcd"),
+				comp.NewLineMatch("1234", "1234"),
+				comp.NewLineMatch("qwerty", "qwerty"),
+			},
+		},
+		{
+			scenario: "Same, With Single Newline, Consecutive Newlines",
+			str:      "abcd\n\n1234",
+			expected: []*comp.LineMatch{
+				comp.NewLineMatch("abcd", "abcd"),
+				comp.NewLineMatch("", ""),
+				comp.NewLineMatch("1234", "1234"),
+			},
+		},
+		{
+			scenario: "Same, With Multi Newline, Complex",
+			str:      "{\n\"key\": \"value\",\n\"stuff\": \"more stuff\"\n}",
+			expected: []*comp.LineMatch{
+				comp.NewLineMatch("{", "{"),
+				comp.NewLineMatch(`"key": "value",`, `"key": "value",`),
+				comp.NewLineMatch(`"stuff": "more stuff"`, `"stuff": "more stuff"`),
+				comp.NewLineMatch("}", "}"),
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		fmt.Println("Running test for scenario: ", tc.scenario)
+		actual := comp.Match(tc.str, tc.str)
+		require.Equal(t, len(tc.expected), len(actual), "Failed: "+tc.scenario)
+
+		for ix, p := range tc.expected {
+			assert.Equal(t, p.A(), actual[ix].A(), "Failed: "+tc.scenario)
+			assert.Equal(t, p.B(), actual[ix].B(), "Failed: "+tc.scenario)
+		}
+	}
+}
+
+func TestMatch_Diff(t *testing.T) {
+	cases := []struct {
+		scenario string
+		a        string
+		b        string
+		expected []*comp.LineMatch
+	}{
+		{
+			scenario: "Different, No Newline, Completely",
+			a:        "abcd",
+			b:        "1234",
+			expected: []*comp.LineMatch{
+				comp.NewLineMatch("abcd", ""),
+				comp.NewLineMatch("", "1234"),
+			},
+		},
+		{
+			scenario: "Different, No Newline, Partially",
+			a:        "abcd",
+			b:        "abc4",
+			expected: []*comp.LineMatch{
+				comp.NewLineMatch("abcd", ""),
+				comp.NewLineMatch("", "abc4"),
+			},
+		},
+		{
+			scenario: "Different, With Newline, First Line",
+			a:        "1234\nxyz",
+			b:        "abcd\nxyz",
+			expected: []*comp.LineMatch{
+				comp.NewLineMatch("1234", ""),
+				comp.NewLineMatch("", "abcd"),
+				comp.NewLineMatch("xyz", "xyz"),
+			},
+		},
+		{
+			scenario: "Different, With Newline, Mid Line",
+			a:        "abcd\n1234\nxyz",
+			b:        "abcd\n5678\nxyz",
+			expected: []*comp.LineMatch{
+				comp.NewLineMatch("abcd", "abcd"),
+				comp.NewLineMatch("1234", ""),
+				comp.NewLineMatch("", "5678"),
+				comp.NewLineMatch("xyz", "xyz"),
+			},
+		},
+		{
+			scenario: "Different, With Newline, Last Line",
+			a:        "abcd\n1234",
+			b:        "abcd\nxyz",
+			expected: []*comp.LineMatch{
+				comp.NewLineMatch("abcd", "abcd"),
+				comp.NewLineMatch("1234", ""),
+				comp.NewLineMatch("", "xyz"),
+			},
+		},
+		{
+			scenario: "Different, With Newline, Partial Criss-Cross Match",
+			a:        "abcd\n1234",
+			b:        "1234\nxyz",
+			expected: []*comp.LineMatch{
+				comp.NewLineMatch("abcd", ""),
+				comp.NewLineMatch("1234", "1234"),
+				comp.NewLineMatch("", "xyz"),
+			},
+		},
+		{
+			scenario: "Different, With Newline, Complete Criss-Cross Match",
+			a:        "abcd\n1234",
+			b:        "1234\nabcd",
+			expected: []*comp.LineMatch{
+				comp.NewLineMatch("", "1234"),
+				comp.NewLineMatch("abcd", "abcd"),
+				comp.NewLineMatch("1234", ""),
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		fmt.Println("Running test for scenario: ", tc.scenario)
+		actual := comp.Match(tc.a, tc.b)
+		require.Equal(t, len(tc.expected), len(actual), "Failed: "+tc.scenario)
+
+		for ix, p := range tc.expected {
+			assert.Equal(t, p.A(), actual[ix].A(), "Failed: "+tc.scenario)
+			assert.Equal(t, p.B(), actual[ix].B(), "Failed: "+tc.scenario)
+		}
+	}
+}
