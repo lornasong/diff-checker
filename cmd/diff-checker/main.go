abc7fe92876c3f91c9d724d46e276b82afa7d646
Lorna Song <lsong@achievementnetwork.org>
2019-04-12 23:34:55 -0400

    update command line display w color and similar lines

--- cmd/diff-checker/main.go
+++ cmd/diff-checker/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	_ "net/http/pprof"
 
+	"github.com/fatih/color"
 	"github.com/lornasong/diff-checker/src/compare"
 	"github.com/pkg/profile"
 )
@@ -14,6 +15,12 @@ const pathToInputFiles = ""
 
 func main() {
 	defer profile.Start(profile.MemProfile).Stop()
+
+	plusColor := color.New(color.FgGreen).SprintFunc()
+	diffColor := color.New(color.FgHiYellow).SprintFunc()
+
+	aColor := color.New(color.FgCyan).SprintFunc()
+	bColor := color.New(color.FgHiMagenta).SprintFunc()
 
 	a, err := ioutil.ReadFile("a.txt")
 	if err != nil {
@@ -33,22 +40,50 @@ func main() {
 
 	lineNumB := 0
 	lineNumA := 0
-	fmt.Println("Check Diff-----")
+	fmt.Println("---------------------------------------------------------------------")
+	fmt.Printf("Diff %s & %s:\n", aColor("A"), bColor("B"))
+	fmt.Println("---------------------------------------------------------------------")
 	for _, line := range lines {
 		if line.Same() {
 			lineNumA++
 			lineNumB++
+			fmt.Printf("L%d/%d A/B\t:%s\n", lineNumA, lineNumB, line.A())
+			continue
+		}
+
+		if line.Similar() {
+			lineNumA++
+			lineNumB++
+			fmt.Printf("L%d/%d %sA/B\t:", lineNumA, lineNumB, diffColor("~"))
+			printSimilarLine(line, aColor, bColor)
 			continue
 		}
 
 		if line.OnlyInA() {
 			lineNumA++
-			fmt.Printf("  Line %d: +A %s\n", lineNumA, line.A())
+			fmt.Printf("L%d %sA\t\t:%s\n", lineNumA, plusColor("+"), aColor(line.A()))
 		}
 		if line.OnlyInB() {
 			lineNumB++
-			fmt.Printf("  Line %d: +B %s\n", lineNumB, line.B())
+			fmt.Printf("L%d %sB\t\t:%s\n", lineNumB, plusColor("+"), bColor(line.B()))
 		}
 	}
-	fmt.Println("End Diff-------")
+	fmt.Println("---------------------------------------------------------------------")
+	fmt.Println("")
+	fmt.Println("---------------------------------------------------------------------")
+}
+
+func printSimilarLine(line *compare.LineMatch, aColor, bColor func(a ...interface{}) string) {
+	for _, w := range line.Children() {
+		if w.Same() {
+			fmt.Printf(w.A())
+		}
+		if w.OnlyInA() {
+			fmt.Printf("%s", aColor(w.A()))
+		}
+		if w.OnlyInB() {
+			fmt.Printf("%s", bColor(w.B()))
+		}
+	}
+	fmt.Println()
 }
