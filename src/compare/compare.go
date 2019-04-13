f9a1d8f3db762147dd88b98f1d668e515ba0f394
Lorna Song <lsong@achievementnetwork.org>
2019-04-12 23:34:11 -0400

    wip: identify similar lines. matching words

--- src/compare/compare.go
+++ src/compare/compare.go
@@ -1,22 +1,44 @@
 package compare
 
 import (
+	"fmt"
 	"strings"
+)
+
+const (
+	newLineDelimiter    = "\n"
+	spaceDelimiter      = " "
+	dashDelimiter       = "-"
+	underScoreDelimiter = "_"
+	commaDelimiter      = ","
+	colonDelimiter      = ":"
+	quotationDelimiter  = "\""
 )
 
 // LineMatch contains text from string a that has been matched to string
 // b up until newline. No match is represented by an empty string
 type LineMatch struct {
-	a string
-	b string
+	a            string
+	b            string
+	childMatches []*LineMatch
 }
 
 // NewLineMatch returns a new LineMatch
-func NewLineMatch(a, b string) *LineMatch {
-	return &LineMatch{
+func NewLineMatch(a, b string, opts ...func(*LineMatch)) *LineMatch {
+	m := &LineMatch{
 		a: a,
 		b: b,
 	}
+
+	for _, opt := range opts {
+		opt(m)
+	}
+
+	return m
+}
+
+func WithChildMatches(child []*LineMatch) func(*LineMatch) {
+	return func(m *LineMatch) { m.childMatches = child }
 }
 
 // A returns the value of string a
@@ -29,10 +51,20 @@ func (p *LineMatch) B() string {
 	return p.b
 }
 
+// A returns the value of string a
+func (p *LineMatch) Children() []*LineMatch {
+	return p.childMatches
+}
+
 // Same returns whether or not the contents of a and b are the same
 // for this line
 func (p *LineMatch) Same() bool {
 	return p.a == p.b
+}
+
+// TODO:
+func (p *LineMatch) Similar() bool {
+	return len(p.childMatches) > 0
 }
 
 // OnlyInA returns whether or not this line only occurs in a
@@ -48,12 +80,127 @@ func (p *LineMatch) OnlyInB() bool {
 // MatchLine returns a list of LineMatchs that represent the matches between string
 // a and string b if any.
 func MatchLine(a, b string) []*LineMatch {
-	return Match(a, b, "\n")
+	return MatchLine2(a, b)
 }
+
+// MatchWord returns a list of LineMatchs that represent the matches between string
+// a and string b if any. Ideally these would be a line
+// func MatchWord(a, b string) []*LineMatch {
+// 	return Match(a, b, " ")
+// }
 
 // Match returns a list of LineMatchs that represent the matches between string
 // a and string b if any.
-func Match(a, b, delim string) []*LineMatch {
+func MatchLine2(a, b string) []*LineMatch {
+
+	aPieces := strings.Split(a, "\n")
+	bPieces := strings.Split(b, "\n")
+
+	matches := make([]*LineMatch, 0)
+	unmatchedB := make([]*LineMatch, 0)
+	matched := false
+
+	// TODO: refactor. think through any optimizations
+	for _, a := range aPieces {
+		matched = false
+		unmatchedB = nil
+
+		// look for matches to b
+		for ixb, b := range bPieces {
+
+			children := sameSimilarLine(a, b)
+			if children != nil {
+				if len(unmatchedB) > 0 {
+					matches = append(matches, unmatchedB...)
+					unmatchedB = nil
+				}
+
+				matched = true
+				matches = append(matches, NewLineMatch(a, b, WithChildMatches(children)))
+
+				// trim off added stuff
+				bPieces = bPieces[ixb+1:]
+				break
+			}
+
+			unmatchedB = append(unmatchedB, NewLineMatch("", b))
+
+		}
+		// there was no match to b
+		if !matched {
+			matches = append(matches, NewLineMatch(a, ""))
+		}
+	}
+
+	// incomplete b
+	for _, b := range bPieces {
+		matches = append(matches, NewLineMatch("", b))
+	}
+
+	return matches
+}
+
+func sameSimilarLine(a, b string) []*LineMatch {
+	fmt.Println("matching lines")
+	fmt.Println("a: ", a)
+	fmt.Println("b: ", b)
+
+	delim := bestDelim(a, b)
+	fmt.Println("delim: ", delim)
+	words := matchWords(a, b, delim)
+	if len(words) == 0 {
+		fmt.Println("not similar (1)")
+		return nil
+	}
+
+	if similar(words, 0.5) {
+		return words
+	}
+	return nil
+}
+
+func bestDelim(a, b string) string {
+	delims := []string{spaceDelimiter, dashDelimiter, underScoreDelimiter, commaDelimiter, colonDelimiter, quotationDelimiter}
+
+	score := 0
+	highestScore := 0
+	highestDelim := spaceDelimiter // default
+	for _, delim := range delims {
+		as := strings.Split(a, delim)
+		bs := strings.Split(b, delim)
+
+		if len(as) < len(bs) {
+			score = len(as)
+		} else {
+			score = len(bs)
+		}
+
+		if score > highestScore {
+			highestScore = score
+			highestDelim = delim
+		}
+	}
+
+	return highestDelim
+}
+
+func similar(matches []*LineMatch, threshold float64) bool {
+	i := 0.0
+	for _, childMatch := range matches {
+		if childMatch.Same() {
+			i++
+		}
+	}
+	percent := i / float64(len(matches))
+	if percent > threshold {
+		fmt.Println("similar", i, len(matches), percent)
+		return true
+	}
+	fmt.Println("not similar (2)", i, len(matches), percent)
+	return false
+}
+
+func matchWords(a, b, delim string) []*LineMatch {
 
 	aPieces := strings.Split(a, delim)
 	bPieces := strings.Split(b, delim)
@@ -70,22 +217,22 @@ func Match(a, b, delim string) []*LineMatch {
 		// look for matches to b
 		for ixb, b := range bPieces {
 
-			if !sameLine(a, b) {
-				unmatchedB = append(unmatchedB, NewLineMatch("", b))
-				continue
+			if sameWord(a, b) {
+				if len(unmatchedB) > 0 {
+					matches = append(matches, unmatchedB...)
+					unmatchedB = nil
+				}
+
+				matched = true
+				matches = append(matches, NewLineMatch(a, b))
+
+				// trim off added stuff
+				bPieces = bPieces[ixb+1:]
+				break
 			}
 
-			if len(unmatchedB) > 0 {
-				matches = append(matches, unmatchedB...)
-				unmatchedB = nil
-			}
+			unmatchedB = append(unmatchedB, NewLineMatch("", b))
 
-			matched = true
-			matches = append(matches, NewLineMatch(a, b))
-
-			// trim off added stuff
-			bPieces = bPieces[ixb+1:]
-			break
 		}
 		// there was no match to b
 		if !matched {
@@ -101,7 +248,9 @@ func Match(a, b, delim string) []*LineMatch {
 	return matches
 }
 
-func sameLine(a, b string) bool {
-	// grow in complexity to be "similarLine"
-	return a == b
+func sameWord(a, b string) bool {
+	if a == b {
+		return true
+	}
+	return false
 }
